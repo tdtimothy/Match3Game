2fe8593f17897d07419008c48c056e315cbde9ba
Timothy To <chaosoulg@gmail.com>
2021-06-21 22:33:22 -0700

    mildly more work

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,6 +6,11 @@ using UnityEngine.Tilemaps;
 public class Movement : MonoBehaviour
 {
     public Tilemap board;
+
+    Vector2 startPos;
+    TileBase holdTile;
+    static int MINIMUM_SWAP_DISTANCE = 32; //32 is placeholder, think about distance wanted later
+    string swapPlaceHold;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +22,43 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
+            startPos = Input.mousePosition;
             Vector3Int mousePos = board.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-            TileBase clickedTile = board.GetTile(mousePos);
-            Debug.Log(clickedTile + " " + mousePos);
+            holdTile = board.GetTile(mousePos);
+            Debug.Log(holdTile + " " + mousePos);
         }
+        if(Input.GetMouseButton(0))
+        {
+            Vector2 swapDir = ((Vector2)Input.mousePosition - startPos);
+            Vector2 normDir = swapDir.normalized;
+            Vector2 absValDir = new Vector2(Mathf.Abs(swapDir.x), Mathf.Abs(swapDir.y));
+            if(swapDir.magnitude > MINIMUM_SWAP_DISTANCE)
+            {
+                if (absValDir.x > absValDir.y)
+                {
+                    if (normDir.x > 0)
+                        swapPlaceHold = "right";
+                    else
+                        swapPlaceHold = "left";
+                }
+                else
+                {
+                    if (normDir.y > 0)
+                        swapPlaceHold = "up";
+                    else
+                        swapPlaceHold = "down";
+                }
+            }
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            swapTiles();
+            swapPlaceHold = "no direction picked";
+        }
+    }
+
+    void swapTiles()
+    {
+        Debug.Log("Swapping tile " + swapPlaceHold);
     }
 }
